3ad086f67f9df84fed9f953b7226f27c6270507d
rohailarshad101 <rohailarshad101@gmail.com>
2024-02-15 00:36:21 +0500

    Create Signup Api Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 // import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn.JSX";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import About from "./pages/About.JSX";
+import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/header";
 
